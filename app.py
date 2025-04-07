dd06b8b30fde380c8c6b9f8094cf8c38dc8774ad
ilyaKrivitskiy <krivitskiy-ilya@mail.ru>
2025-04-07 16:37:29 +0300

    issues fixed; http error handlers added

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify
 import redis
 import os
 
+from pyexpat import expat_CAPI
+
 app = Flask(__name__)
 
 redis_host = os.getenv("REDIS_HOST", "localhost")
@@ -14,8 +16,19 @@ def ping():
 
 @app.route('/count', methods=['GET'])
 def count():
-    visits = redis_client.incr("visit_count")
-    return jsonify({"visit_count": visits})
+    try:
+        visits = redis_client.incr("visit_count")
+        return jsonify({"visit_count": visits})
+    except redis.exceptions.ConnectionError:
+        return jsonify({'error': 'Redis is not available'}), 500
+
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify({'error': 'Not found'}), 404
+
+@app.errorhandler(500)
+def internal_error(e):
+    return jsonify({'error': 'Internal server error'}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
