f72af798d4a6130261e9310f2f21fd71a508c101
ilyaKrivitskiy <krivitskiy-ilya@mail.ru>
2025-04-07 16:09:03 +0300

    issues fixed; docker compose added; redis configured; task 2 solved

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, jsonify
+import redis
+import os
 
 app = Flask(__name__)
+
+redis_host = os.getenv("REDIS_HOST", "localhost")
+redis_port = int(os.getenv("REDIS_PORT", 6379))
+redis_client = redis.Redis(host=redis_host, port=redis_port, db=0, decode_responses=True)
 
 @app.route('/ping', methods=['GET'])
 def ping():
     return jsonify({"status": "ok"})
+
+@app.route('/count', methods=['GET'])
+def count():
+    visits = redis_client.incr("visit_count")
+    return jsonify({"visit_count": visits})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
